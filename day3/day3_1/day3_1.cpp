761cb5dbe2b60ecf0953f89edd02bdbef9fcd47b
Thomas Neff <thomas.neff@icg.tugraz.at>
2020-12-03 09:52:41 +0100

    Nice 2d map task for day 3, pretty fun!

---
+++ day3/day3_1/day3_1.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+enum class TileType
+{
+	Tree,
+	Empty
+};
+
+class TreeMap
+{
+public:
+	// This assumes everything about the input is not malformed, there is no error handling at all.
+	TreeMap(std::vector<std::string> all_lines_from_file) : width(all_lines_from_file[0].size()), height(all_lines_from_file.size())
+	{
+		// allocate vector from width and height
+		tiles.resize(width * height);
+
+		// copy elements into map
+
+		for(int y = 0; y < height; y++)
+		{
+			const auto& line = all_lines_from_file[y];
+			for(int x = 0; x < width; x++)
+			{
+				setFromXYChar(line[x], x, y);
+			}
+		}
+	}
+
+	TileType getFromXY(int x, int y) { return tiles[x + width * y]; }
+
+	void setFromXYChar(char tile, int x, int y)
+	{
+		TileType t = TileType::Empty;
+
+		if(tile == '#')
+			t = TileType::Tree;
+
+		tiles[x + width * y] = t;
+	}
+
+	int getHeight() { return height; }
+
+	int getWidth() { return width; }
+
+private:
+	std::vector<TileType> tiles;
+	int width;
+	int height;
+};
+
+int task_map_iteration(TreeMap& tree_map)
+{
+	int height = tree_map.getHeight();
+	int width = tree_map.getWidth();
+
+	int pos_x = 0;
+	int pos_y = 0;
+	int num_trees = 0;
+
+	while(pos_y < height)
+	{
+		// Check tree, repeat in x via modulo
+		if(tree_map.getFromXY(pos_x % width, pos_y) == TileType::Tree)
+			num_trees++;
+
+		// Increase 3 x, 1 y according to task
+		pos_x += 3;
+		pos_y += 1;
+	}
+
+	return num_trees;
+}
+
+int main()
+{
+	std::ifstream input_file("input.txt");
+
+	std::string input_line;
+
+	std::vector<std::string> input_lines;
+
+	// task: read 2d map, start top left, go 3 right 1 down each "iteration", check how many trees are hit
+	//       map repeats horizontally, end -> when bottom is hit
+
+	// get all input numbers from file
+	while(std::getline(input_file, input_line))
+	{
+		input_lines.push_back(input_line);
+	}
+
+	TreeMap tree_map(input_lines);
+
+	// Now we can iterate the tree map according to the algorithm
+	std::cout << "Number of trees encountered: " << task_map_iteration(tree_map) << "\n";
+}
